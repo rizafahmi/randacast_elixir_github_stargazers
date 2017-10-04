e8cf1b4eab35c233f04ec5daf23be3570c5b760b
Riza Fahmi <rizafahmi@gmail.com>
2017-10-04 10:26:56 +0700

    Get github stats

---
+++ lib/minion.ex
@@ -0,0 +1,29 @@
+defmodule GithubStargazers.Minion do
+  def get_stats(username) do
+    result = url_for(username) |> HTTPoison.get() |> parse_response()
+
+    case result do
+      {:ok, name, followers, following} ->
+        "#{username}: #{name} follows #{following}, got #{followers} follower(s)."
+      :error ->
+        "#{username} not found, sorry."
+    end
+  end
+
+  def url_for(username) do
+    url = URI.encode(username)
+    "https://api.github.com/users/#{url}"
+  end
+
+  def parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> get_followers
+  end
+
+  def parse_response(_) do
+    :error
+  end
+
+  def get_followers(json) do
+    {:ok, json["name"], json["followers"], json["following"]}
+  end
+end
